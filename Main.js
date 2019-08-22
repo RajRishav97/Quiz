9595bafa5c90aa3fa5d49cc55d88d36f8510cb1a
Rishav.Kumar <rishav.kumar@cgi.com>
2019-08-22 14:38:05 +0530

    Welcome to the quiz.

---
+++ Main.js
@@ -0,0 +1,54 @@
+
+
+  function html(){
+    var x = document.createTextNode("Let's ready to begin the quiz of HTML.");
+    var parent = document.getElementById("main_page");
+    var child = document.getElementById("start_quiz");
+    var par=document.getElementById("content");
+    var imp=document.getElementById("importance");
+    var frm=document.getElementById("form");
+    document.body.removeChild(imp);
+    document.body.removeChild(frm);
+    child.appendChild(x);
+    parent.removeChild(par);
+  }
+
+  function css(){
+    var x = document.createTextNode("Let's ready to begin the quiz of CSS.");
+    var parent = document.getElementById("main_page");
+    var child = document.getElementById("start_quiz");
+    var par=document.getElementById("content");
+    var imp=document.getElementById("importance");
+    var frm=document.getElementById("form");
+    document.body.removeChild(imp);
+    document.body.removeChild(frm);
+    child.appendChild(x);
+    parent.removeChild(par);
+  }
+
+  function js(){
+    var x = document.createTextNode("Let's ready to begin the quiz of JAVASCRIPT.");
+    var parent = document.getElementById("main_page");
+    var child = document.getElementById("start_quiz");
+    var par=document.getElementById("content");
+    var imp=document.getElementById("importance");
+    var frm=document.getElementById("form");
+    document.body.removeChild(imp);
+    document.body.removeChild(frm);
+    child.appendChild(x);
+    parent.removeChild(par);
+  }
+
+  function sql(){
+    var x = document.createTextNode("Let's ready to begin the quiz of SQL.");
+    var parent = document.getElementById("main_page");
+    var child = document.getElementById("start_quiz");
+    var par=document.getElementById("content");
+    var imp=document.getElementById("importance");
+    var frm=document.getElementById("form");
+    document.body.removeChild(imp);
+    document.body.removeChild(frm);
+    child.appendChild(x);
+    parent.removeChild(par);
+    document
+  }
